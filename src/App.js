2713695a3d77b2bcc342982d3f7d1836e2a48d65
vyslbyrx <58915055+vyslbyrx@users.noreply.github.com>
2022-08-12 14:09:13 +0300

    proje ekle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import { Link,Outlet} from "react-router-dom"
+
 import './App.css';
 
-function App() {
+
+export function Home() {
+    return(
+        <div>
+            <Link to="/about" >About</Link>
+            <Link to="/contactus" >Contact</Link>
+            <Link to="/about/history" >History</Link>
+            <h1>My Home</h1>
+        </div>
+    )
+}
+
+export function About() {
+    return(
+        <div>
+            <Link to="/" >Home</Link>
+            <Link to="/contactus" >Contact</Link>
+            <Link to="/about/history" >History</Link>
+            <Outlet />
+            <h1>About Us</h1>
+
+        </div>
+    )
+}
+
+export function History() {
+    return(
+        <div>
+            <h1>Geçmişimiz sayfası Hakkımızda Sayfasının bir alt sayfasıdır</h1>
+        </div>
+    )
+}
+
+export function Contact() {
+    return(
+        <div>
+            <Link to="/" >Home</Link>
+            <Link to="/about" >About</Link>
+            <Link to="/about/history" >History</Link>
+            <h1>Contact Us</h1>
+        </div>
+    )
+}
+
+export function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Link to="/home" >Home</Link>
+        <Link to="/about" >About</Link>
+        <Link to="/about/history" >History</Link>
+<h1>Merbaa Reizzz</h1>
     </div>
   );
 }
 
-export default App;
+
